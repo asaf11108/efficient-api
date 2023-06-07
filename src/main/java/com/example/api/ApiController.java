07b0cf7955e05e18be634915d664656404238d91
Asaf Regev <asaf.regev@shieldfc.com>
2023-06-07 08:42:00 +0300

    raw query params

--- src/main/java/com/example/api/ApiController.java
+++ src/main/java/com/example/api/ApiController.java
@@ -1,19 +1,20 @@
 package com.example.api;
 
-import java.util.Map;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping(path = "api")
 public class ApiController {
 
     @GetMapping
-    public ApiResponseDto getApi(@RequestParam Map<String, ?> params, Pageable pageable) {
-        return ApiResponseDto.builder().filters(params).pageable(pageable).build();
+    // Other option to get raw query params
+    // @Context UriInfo uriInfo
+    // uriInfo.getRequestUri().getQuery()
+    public String getApi(HttpServletRequest request) {
+        return request.getQueryString();
     }
 }
